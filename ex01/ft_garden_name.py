de9ff2bc936ceb6ba18418062dc68a635db08461
Vicente Lourenco Prista Von Bonhorst <vlourenc@c1r1s1.42lisboa.com>
2026-05-25 13:12:51 +0100

    done

--- ex01/ft_garden_name.py
+++ ex01/ft_garden_name.py
@@ -1,6 +1,4 @@
 def ft_garden_name():
-    name = input("Enter garden name:")
-    print(f"Garden name: {name}")
-
-
-print("Status: Growing well!")
+    name = input("Enter garden name: ")
+    print(f"Garden : {name}")
+    print("Status: Growing well!")
