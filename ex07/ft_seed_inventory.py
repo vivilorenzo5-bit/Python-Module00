95af67c5475cec43f8908eba0da24bb6a56b0e09
Vicente Lourenco Prista Von Bonhorst <vlourenc@c1r1s1.42lisboa.com>
2026-05-27 10:57:49 +0100

    dpone

--- ex07/ft_seed_inventory.py
+++ ex07/ft_seed_inventory.py
@@ -1,10 +1,10 @@
-def ft_seed_inventory(seed_type: str, quantity: int, unit: str):
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+    seed_name = seed_type.capitalize()
     if unit == "packets":
-        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} available")
+        print(f"{seed_name} seeds: {quantity} {unit} available")
     elif unit == "grams":
-        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} total")
+        print(f"{seed_name} seeds: {quantity} {unit} total")
     elif unit == "area":
-        print(f"{seed_type.capitalize()} seeds: covers {quantity} \
-square meters")
+        print(f"{seed_name} seeds: covers {quantity} square meters")
     else:
         print("Unknown unit type")
